c2fe40ec4afe9b607faba8fa0f6e7bd19b86be25
Reniat <reniat314@gmail.com>
2019-01-08 01:45:27 -0600

    Adding comments

--- DogCommand.js
+++ DogCommand.js
@@ -1,16 +1,38 @@
 const DogPicDAO = require('./DogPicDAO');
+const Logger = require('./Logger');
 const dogPicDAO = new DogPicDAO();
 
 class DogCommand {
+
+  /**
+   * Constructs a DogCommand object.
+   * 
+   * @param {String} dog - The dog on which to process the commands. This cannot be empty or undefined.
+   */
   constructor(dog) {
     this.dog = dog;
   }
 
+  /**
+   * Takes a command and processes it in relation to the dog set on the DogCommand object. This will do nothing if the DogCommand object was created with an empty or undefined dog.
+   * 
+   * @param {String} message - The full command to process
+   */
   processMessage(message) {
+    if (this.dog) {
+      Logger.logEv
+      return;
+    }
     handleDogCommand(message, this.dog);
   }
 }
 
+/**
+ * Processes the command in relation to the given dog.
+ * 
+ * @param {String} message - The full command to process
+ * @param {String} dog - The dog to use as refererence when processing the command
+ */
 function handleDogCommand(message, dog) {
 
   var content = message.content;
@@ -44,10 +66,22 @@ function handleDogCommand(message, dog) {
   sendRandomDog(message.channel, dog);
 }
 
+/**
+ * Sends a message to the given channel that there are no dog pics for the given dogName
+ * 
+ * @param {Object} channel - Channel to send the message to
+ * @param {String} dogName - Name of the dog to use in the message
+ */
 function noDogsMessage(channel, dogName) {
   channel.send("Unfortunately there are no " + dogName + " pics to show currently :(\nA user with valid permissions can add pics with '!" + dogName + " -add <link to image>'")
 }
 
+/**
+ * Sends a random picture from the pool of images for the given dog, to the given channel.
+ * 
+ * @param {Object} channel - The channel to send the dog pic
+ * @param {String} dog - The dog whose pool of images to use
+ */
 function sendRandomDog(channel, dog) {
   dogPicDAO.getDogPics(dog, function (item) {
     var dogPics = item.dogPics;
@@ -56,6 +90,12 @@ function sendRandomDog(channel, dog) {
   });
 }
 
+/**
+ * Processes the case where a doc pic is to be added.
+ * 
+ * @param {String} message - The full command
+ * @param {String} dog - The dog to use as a reference for the command
+ */
 function handleAddCase(message, dog) {
   if (!validAddRole(message.member)) {
     message.channel.send("Sorry, you do not have the right role to add dog pics.")
@@ -76,11 +116,23 @@ function handleAddCase(message, dog) {
   });
 }
 
+/**
+ * Checks whether a given member has a valid role for adding dog pics.
+ * 
+ * @param {Object} member - The member to validate
+ * @returns {boolean} true if the member has a valid role, false otherwise
+ */
 function validAddRole(member) {
   var dogCommandRoleID = "523526927844376577";
   return member.roles.has(dogCommandRoleID);
 }
 
+/**
+ * Parses a string of comma seperated images and returns a list of the images.
+ * 
+ * @param {String} imagesString 
+ * @returns {String[]} array of images contained in the gtiven string
+ */
 function parsePics(imagesString) {
   splitImages = imagesString.split(",");
   images = splitImages.length != 0 ? splitImages : imagesString;
